47ad2c4242ce73967276b96a171c94535cfa678a
Emre Surmeli <s.emre.s.8@gmail.com>
2015-05-19 22:15:48 -0700

    ///

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Set dependencie 
+// Set dependencies
 var express = require('express');
 var app = express();
 var userRoutes = express.Router();
@@ -9,6 +9,7 @@ require('./routes/routes')(userRoutes);
 
 app.use('/api', userRoutes);
 
+// run app on localhost:3000
 app.listen(process.env.PORT || 3000, function() {
 	console.log('server is running on localhost:3000');
 });
